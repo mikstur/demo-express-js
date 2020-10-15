45afcd7881c89609e253de723c233f6268eca40f
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2020-10-15 12:01:45 +0200

    Testing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('static'))
+app.use(express.static("static"));
 app.use(express.json());
 
 app.get("/api/data", (req, res) => {
@@ -13,17 +13,17 @@ app.get("/api/data", (req, res) => {
     data: [
       { id: 1, title: "Some data" },
       { id: 2, title: "Some other data" },
+      { id: 3, title: "Testing" },
     ],
   });
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get("/index", (req, res) => {
-  res.redirect("/")
+  res.redirect("/");
 });
-
 
 app.listen(port, () => console.log(`Listening on ${port}`));
