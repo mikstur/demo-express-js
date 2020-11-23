96ce5d6b8cd2355ce8ed2b017197bd4c9990513b
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2020-11-23 14:35:14 +0200

    A change

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get("/api/data", (req, res) => {
       { id: 2, title: "Some other data" },
       { id: 3, title: "Testing" },
       { id: 4, title: "Push Test" },
+      { id: 6, title: "New test" },
       { id: 5, title: process.env.TEST_ENV },
     ],
   });
